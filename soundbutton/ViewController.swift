b8f61e30568eb817de13117309aadba30d1044a2
valeriorosssetti <lamagioviale@gmail.com>
2016-11-20 18:09:21 +0100

    first version
    first version with only one button to stop and play

---
+++ soundbutton/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  soundbutton
+//
+//  Created by ValerioRossetti on 20/11/16.
+//  Copyright © 2016 ValerioRossetti. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    var url = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("george", ofType: "mp3")!)
+    var audioplayer = AVAudioPlayer()
+    var counter = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        do{
+            audioplayer = try AVAudioPlayer(contentsOfURL:url, fileTypeHint: nil)
+        }catch
+        {
+         }            // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playmyass(sender: AnyObject){
+        if counter == 0 {
+            audioplayer.prepareToPlay()
+            audioplayer.currentTime = 0
+            audioplayer.play()
+            counter = 1
+        }
+        else{
+            audioplayer.stop()
+            counter = 0
+        }
+    }
+    
+    
+    
+}
+
